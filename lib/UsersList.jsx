0bc9b25e3fd0aac5eaa65a5ad77b04a45ba7ffca
Andrew Virts <asvirts@outlook.com>
2023-10-14 16:42:13 -0500

    added auth

---
+++ lib/UsersList.jsx
@@ -0,0 +1,30 @@
+"use client";
+
+import { useEffect, useState } from "react";
+import { supabase } from "./initSupabase";
+
+export default function UsersList() {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    fetchUsers();
+  }, []);
+
+  const fetchUsers = async () => {
+    const { data: users } = await supabase
+      .from("users")
+      .select("*")
+      .order("id", true);
+    setUsers(users);
+  };
+
+  return (
+    <ul>
+      {users ? (
+        users.map((user) => <li key={user.id}>{user.name}</li>)
+      ) : (
+        <li>No users available</li>
+      )}
+    </ul>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,14 @@
 import AdbIcon from "@mui/icons-material/Adb";
+import UsersList from "../../lib/UsersList";
+import AuthForm from "./AuthForm";
 
 export default function Home() {
   return (
     <main className="container flex mx-auto justify-center">
       <h1 className="text-4xl font-bold">Spark</h1>
       <AdbIcon />
+      <UsersList />
+      <AuthForm />
     </main>
   );
 }
